fbe7bc6e8ff6fef4e861a3588c8d231a1f171197
sarhana-adh <72303952+sarhana-adh@users.noreply.github.com>
2024-05-28 21:33:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// document.getElementById("insta").addEventListener("mouseover", mouseOver);
+// document.getElementById("insta").addEventListener("mouseout", mouseOut);
+
+// document.getElementById("email").addEventListener("mouseout", mouseOut);
+// document.getElementById("email").addEventListener("mouseenter", mouseOver(2));
+// document.getElementById("linkedin").addEventListener("mouseout", mouseOut);
+// document.getElementById("linkedin").addEventListener("mouseenter", mouseOver(3));
+// document.getElementById("github").addEventListener("mouseout", mouseOut);
+// document.getElementById("github").addEventListener("mouseenter", mouseOver(4));
+
+// function mouseOut() {
+//     element1 = document.getElementById("social_id").lastChild;
+//     element1.remove();
+
+// }
+
+// function mouseOver() {
+//     element2 = document.getElementById("social_id");
+//     element2.innerHTML += "<h3 id=&#39addition&#39> My name is Sarhana </h3>";
+
+// }
